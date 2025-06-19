0991aa2e9665bfc4da426c5730adba4062117747
pardhu-423141 <pardhukatakam25@gmail.com>
2025-06-19 22:47:15 +0530

    firestore connected

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+// android/build.gradle.kts
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0") // Firebase plugin
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -12,6 +24,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
